5cc7a01e7afc62f78602e5d2e9fbcd0e7ce3ee74
Swiety <!Q2w3e4r>
2018-02-12 23:59:43 +0100

    Added ErrorController

--- src/main/java/com/advisor/configuration/SecurityConfiguration.java
+++ src/main/java/com/advisor/configuration/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package com.advisor.configuration;
 
 import javax.sql.DataSource;
 
+import com.advisor.controllers.AppErrorController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +44,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.dataSource(dataSource)
 				.passwordEncoder(bCryptPasswordEncoder);
 	}
+
+	@Autowired
+	private ErrorAttributes errorAttributes;
+
+	@Bean
+	public AppErrorController appErrorController(){return new AppErrorController(errorAttributes);}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
