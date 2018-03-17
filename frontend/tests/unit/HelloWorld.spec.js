2d0cf553b568a7415f969cc57ad4441ddea47c4b
Bartosz Baczek <bartosz.baczek@gmail.com>
2018-03-17 15:33:19 +0100

    fulfill linter rules

--- frontend/tests/unit/HelloWorld.spec.js
+++ frontend/tests/unit/HelloWorld.spec.js
@@ -2,11 +2,11 @@ import { shallow } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallow(HelloWorld, {
-      propsData: { msg },
+    it('renders props.msg when passed', () => {
+        const msg = 'new message';
+        const wrapper = shallow(HelloWorld, {
+            propsData: { msg },
+        });
+        expect(wrapper.text()).toMatch(msg);
     });
-    expect(wrapper.text()).toMatch(msg);
-  });
 });
