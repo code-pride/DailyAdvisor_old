7f62511580d35234cced1553cac6667fa2731f40
BartoszBaczek <bartoszbaczek@gmail.com>
2018-03-26 20:55:35 +0200

    lot of stuff done - added vuetify, added theme, added basic markup for login component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+    theme: {
+        primary: '#03A9F4',
+        secondary: '#BF360C',
+        accent: '#64DD17',
+        error: '#f44336',
+        warning: '#ffeb3b',
+        info: '#2196f3',
+        success: '#4caf50',
+    },
+});
 
 new Vue({
     router,
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="background">
+        <div class="login-card-wrapper">
+            <v-card class="card-content">
+                <v-form>
+                    <v-text-field
+                        label="Email"
+                        v-model="email"
+                        :rules="emailRules"
+                    ></v-text-field>
+                    <v-text-field
+                        label="Password"
+                        v-model="password"
+                        :rules="passwordRules"
+                        :type="'password'"
+                    ></v-text-field>
+                </v-form>
+                <v-btn color="secondary">Log in</v-btn>
+                <v-btn flat small color="primary">Sign up</v-btn>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .background {
+        background: #29D8F3;
+        // background: red;
+        display: flex;
+        flex: 1 1 0;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .login-card-wrapper {
+        height: 300px;
+        margin: 0 auto;
+        width: 90%;
+    }
+
+    .card-content {
+        display: flex;
+        flex: 1 1 0;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
