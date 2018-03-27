79cff3915e521fa5edaf9c69657c9118d315215f
Makowski Marek <makow.mark@gmail.com>
2018-03-27 19:03:36 +0200

    Update SecurityConfiguration.java

--- backend/src/main/java/com/advisor/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/advisor/configuration/SecurityConfiguration.java
@@ -20,6 +20,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
