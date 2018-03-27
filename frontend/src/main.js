90eb52b0ef133cae482e3fb478afabdf9b4257b4
marckraw <marckraw@icloud.com>
2018-03-27 20:14:43 +0200

    Some first try with, forms and vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
     router,
