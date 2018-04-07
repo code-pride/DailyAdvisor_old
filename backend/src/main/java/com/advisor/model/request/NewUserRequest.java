50a282a8c10b5a4868d27771d2b5b33860b7a9fe
Marcin Krawczyk <marckraw@icloud.com>
2018-04-07 17:28:06 +0200

    Add user type for registration

--- backend/src/main/java/com/advisor/model/request/NewUserRequest.java
+++ backend/src/main/java/com/advisor/model/request/NewUserRequest.java
@@ -21,16 +21,20 @@ public class NewUserRequest {
     @NotBlank
     private String password;
 
+    @NotBlank
+    private String userType;
+
 
     public NewUserRequest() {
     }
 
-    public NewUserRequest(String email, String name, String lastName, String city, String password) {
+    public NewUserRequest(String email, String name, String lastName, String city, String password, String userType) {
         this.email = email;
         this.name = name;
         this.lastName = lastName;
         this.city = city;
         this.password = password;
+        this.userType = userType;
     }
 
 }
