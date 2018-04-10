41bf3c876bfcee77fbff0bf13dc915f1a1495503
bartoszbaczek <bartoszbaczek@gmail.com>
2018-04-10 23:28:57 +0200

    frontend part of logging with facebook is almost ready - still need to find a way to work with http when on localhost

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,7 +1,10 @@
+/* global FB */
+
 const INCORRECT_CREDENTIALS_ERROR = 'Incorrect email or password.';
 
 const authService = {
     login(credentials) {
+        console.log(FB);
         // temporar solution, as API doesn't work yet
         return new Promise((resolve, reject) => {
             if (credentials.email === 'm@m.mm' && credentials.password === '111111') {
@@ -10,6 +13,16 @@ const authService = {
                 reject(INCORRECT_CREDENTIALS_ERROR);
             }
         });
+    },
+    loginWithFacebook() {
+        FB.getLoginStatus(
+            (response) => {
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+            },
+        );
     },
 };
 
