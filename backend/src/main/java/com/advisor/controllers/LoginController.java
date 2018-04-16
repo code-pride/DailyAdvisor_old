0b63d9c7b18e120f5948cec9af350b3a0212e636
makowskimarek <makow.mark@gmail.com>
2018-04-16 22:53:26 +0200

    Added simply mail sending

--- backend/src/main/java/com/advisor/controllers/LoginController.java
+++ backend/src/main/java/com/advisor/controllers/LoginController.java
@@ -1,12 +1,15 @@
 package com.advisor.controllers;
 
 import javax.validation.Valid;
+
+import com.advisor.mail.EmailService;
 import com.advisor.model.entity.User;
 import com.advisor.model.request.NewUserRequest;
 import com.advisor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +17,12 @@ public class LoginController {
 	
 	@Autowired
 	private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    public SimpleMailMessage template;
 	
 	@RequestMapping(value = "/registration", method = RequestMethod.POST)
 	public ResponseEntity createNewUser(@Valid @RequestBody NewUserRequest newUserRequest) {
@@ -24,6 +33,10 @@ public class LoginController {
                 return new ResponseEntity(HttpStatus.OK);
             } else if ("coach".equals(newUserRequest.getUserType())){
                 userService.registerCoach(newUserRequest);
+
+                String text = template.getText();
+                emailService.sendSimpleMessage("swietoszeczek@gmail.com", "test_DA", text);
+
                 return new ResponseEntity(HttpStatus.OK);
             }
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
@@ -41,4 +54,7 @@ public class LoginController {
     public ResponseEntity hello() {
         return new ResponseEntity(HttpStatus.OK);
     }
+
+
+
 }
