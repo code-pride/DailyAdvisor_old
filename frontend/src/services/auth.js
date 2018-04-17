eb570fdcf31348f67c1d11036833461e2b72542b
bartoszbaczek <bartoszbaczek@gmail.com>
2018-04-17 18:49:29 +0200

    removed console log

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -4,7 +4,6 @@ const INCORRECT_CREDENTIALS_ERROR = 'Incorrect email or password.';
 
 const authService = {
     login(credentials) {
-        console.log(FB);
         // temporar solution, as API doesn't work yet
         return new Promise((resolve, reject) => {
             if (credentials.email === 'm@m.mm' && credentials.password === '111111') {
