ecaf632bc56a07dd8833e733bdd3b0ef02b8d818
Marcin Krawczyk <marckraw@icloud.com>
2018-04-24 18:57:41 +0200

    Fix problem with npm run serve and babel version, add registrationConfigrm component

---
+++ frontend/src/views/RegistrationConfirm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sample-div">
+        Kliknales w link. Jestes bardziej pojebany niż myslalem! {{ sampleData }}
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        sampleData: 'asdasdas',
+    }),
+
+    methods: {
+        sampleMethod() {
+            console.log('Sample message');
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .sample-div {
+        color: red;
+    }
+</style>
