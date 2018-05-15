f2df85a874bae7be72e4d90d0b0aba995796a7f1
bartoszbaczek <bartoszbaczek@gmail.com>
2018-05-15 19:56:59 +0200

    test

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -9,20 +9,62 @@ const FACEBOOK_LOGIN_FAILED = 'Facebook login failed, as it is not implemented y
 const authService = {
     login(credentials) {
         // temporar solution, as API doesn't work yet
+        console.log(credentials);
+        console.log(document.cookie);
         return new Promise((resolve, reject) => {
-            if (credentials.email === 'm@m.mm' && credentials.password === '111111') {
-                resolve();
-            } else {
-                reject(INCORRECT_CREDENTIALS_ERROR);
-            }
+            console.log(authService.getCookie('XSRF-TOKEN'));
+            const request = axios.create({
+                // withCredentials: true,
+                headers: {
+                    'XSRF-TOKEN': authService.getCookie('XSRF-TOKEN'),
+                },
+            });
+
+            request.post(`${apiUrl}/login`, {
+                username: credentials.email,
+                password: credentials.password,
+            }).then(
+                () => {
+                    console.log('SUCCES!');
+                    resolve();
+                },
+                (error) => {
+                    console.log('nie pyklo')
+                    console.log(error);
+                    reject();
+                },
+            );
+
+            // if (credentials.email === 'm@m.mm' && credentials.password === '111111') {
+            //     resolve();
+            // } else {
+            //     reject(INCORRECT_CREDENTIALS_ERROR);
+            // }
         });
     },
     loginWithGoogle() {
         return new Promise((resolve, reject) => {
             // lets just see what will happen here
-            axios.get(`${apiUrl}/login/google`).then(
-                () => resolve(),
-                () => reject(GOOGLE_LOGIN_FAILED),
+            const request = axios.create({
+                withCredentials: true,
+                // headers: {
+                //     'Access-Control-Allow-Origin': '*',
+                // },
+                // params: {
+                //     id: 37880978,
+                //     updateTime: -1,
+                // },
+            });
+
+            request.get(`${apiUrl}/csrf`).then(
+                () => {
+                    console.log(document.cookie);
+                    resolve();
+                },
+                () => {
+                    console.log(document.cookie);
+                    reject(GOOGLE_LOGIN_FAILED);
+                },
             );
         });
     },
@@ -34,6 +76,15 @@ const authService = {
                 () => reject(FACEBOOK_LOGIN_FAILED),
             );
         });
+    },
+    getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) {
+            return parts.pop().split(';').shift();
+        }
+
+        return 'spierdalaj';
     },
 };
 
