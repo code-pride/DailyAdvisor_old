4ded61c4d434fc0847c8ce541a401fa26708baa2
Marcin Krawczyk <marckraw@icloud.com>
2018-07-17 21:10:40 +0200

    Login page some styling and stuff

---
+++ frontend/src/pages/Login/components/LoginForm/index.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+import Button from '../../../../components/Button';
+
+class LoginForm extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <label htmlFor="username">Username</label>
+                <input id="username" value={this.state.username} type="text" />
+
+                <label htmlFor="username">Password</label>
+                <input id="password" value={this.state.password} type="password" />
+
+                <Button url="/main" content="Zaloguj" />
+            </Fragment>
+        );
+    }
+}
+
+export default LoginForm;
