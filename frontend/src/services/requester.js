8009a9d63c59436242ddd48c61a2993e0c4dae00
Tomasz Ferens <tomaszferens@hotmail.com>
2018-07-18 17:31:44 +0200

    configure redux-observable and remove redux-saga (#63)

--- frontend/src/services/requester.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-
-export const requestRandomUser = () => axios.get('https://randomuser.me/api/');
--- frontend/src/store/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import getRandomUserSaga from './randomUserSaga';
-import authSaga from '../../auth/sagas';
-
-export default function* rootSaga() {
-    yield all([getRandomUserSaga(), authSaga()]);
-}
