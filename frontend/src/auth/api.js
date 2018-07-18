0fc449b628a287a49cd108229fda4d01fe867376
Tomasz Ferens <tomaszferens@hotmail.com>
2018-07-18 16:54:24 +0200

    configure redux-observable and remove redux-saga

--- frontend/src/auth/api.js
+++ frontend/src/auth/api.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
-
 import { http } from '../../http';
-
-export const requestRandomUser = () => axios.get('https://randomuser.me/api/');
 
 const registerUser = user => http.post('/registration', user);
 
--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,30 +1,11 @@
 import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
 
-import { FETCH_RANDOM_USER_REQUESTED } from '../../store/actions/randomUserActions';
-
-import Button from '../../components/Button';
-
-class Main extends React.Component {
-    trySaga = () => {
-        this.props.dispatch({ type: FETCH_RANDOM_USER_REQUESTED });
-    };
-
+export default class Main extends React.Component {
     render() {
         return (
             <Fragment>
                 <div>Main</div>
-                <Button onClick={this.trySaga} content="try saga" />
             </Fragment>
         );
     }
 }
-
-const mapStateToProps = state => ({
-    userData: state.randomUserReducer.userData,
-});
-
-export default connect(
-    mapStateToProps,
-    null,
-)(Main);
