18b79d875c1f5e9d3a838c8914906d4aa8e824fa
Tomasz Ferens <tomaszferens@hotmail.com>
2018-07-21 16:53:50 +0200

    Add basic calendar module, display number of days in calendar (#65)

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
+import { Route } from 'react-router';
+
+import Calendar from '../../calendar';
 
 export default class Main extends React.Component {
     render() {
         return (
             <Fragment>
                 <div>Main</div>
+                <Route path="/main/calendar" component={Calendar} />
             </Fragment>
         );
     }
